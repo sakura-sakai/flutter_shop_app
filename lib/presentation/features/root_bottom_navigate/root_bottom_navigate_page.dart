363e890cedd0da06887916a1c83fcb2256e208ce
sakura-sakai <sakura.sakai.99@gmail.com>
2022-07-13 16:18:20 +0700

    Refactor root_bottom_navigate_page.dart

--- lib/presentation/features/root_bottom_navigate/root_bottom_navigate_page.dart
+++ lib/presentation/features/root_bottom_navigate/root_bottom_navigate_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../../l10n/l10n.dart';
 import '../../route/router.dart';
+import '../../theme/theme.dart';
 
 class RootBottomNavigatePage extends HookWidget {
   const RootBottomNavigatePage({Key? key}) : super(key: key);
@@ -19,34 +21,49 @@ class RootBottomNavigatePage extends HookWidget {
       ],
       bottomNavigationBuilder: (context, tabsRouter) {
         return BottomNavigationBar(
-          currentIndex: tabsRouter.activeIndex,
           showSelectedLabels: false,
           showUnselectedLabels: false,
+          currentIndex: tabsRouter.activeIndex,
+          type: BottomNavigationBarType.fixed,
+          fixedColor: AppColors.violet,
           onTap: tabsRouter.setActiveIndex,
           iconSize: 20,
           items: [
-            BottomNavigationBarItem(
-              icon: const Icon(
-                Icons.home,
-                size: 23.0,
-              ),
+            _NavigationItem(
+              icon: CupertinoIcons.house,
+              activeIcon: CupertinoIcons.house_fill,
               label: L10n.current.home,
             ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.shop),
+            _NavigationItem(
+              icon: CupertinoIcons.square_stack_3d_up,
+              activeIcon: CupertinoIcons.square_stack_3d_up_fill,
               label: L10n.current.brand,
             ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.shopping_cart),
-              label: L10n.current.brand,
+            _NavigationItem(
+              icon: CupertinoIcons.bag,
+              activeIcon: CupertinoIcons.bag_fill,
+              label: L10n.current.cart,
             ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.person),
-              label: L10n.current.brand,
+            _NavigationItem(
+              icon: CupertinoIcons.person,
+              activeIcon: CupertinoIcons.person_fill,
+              label: L10n.current.myPersonal,
             ),
           ],
         );
       },
     );
   }
+}
+
+class _NavigationItem extends BottomNavigationBarItem {
+  _NavigationItem({
+    required IconData icon,
+    required IconData activeIcon,
+    required String label,
+  }) : super(
+          icon: Icon(icon, size: 23),
+          activeIcon: Icon(activeIcon, size: 23),
+          label: label,
+        );
 }
